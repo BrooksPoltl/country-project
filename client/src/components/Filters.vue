ebd994b3764cd97747d316f97fe666a9af7af60f
brookspoltl <bpoltl1@gmail.com>
2019-11-14 15:46:29 -0600

    update

---
+++ client/src/components/Filters.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <app-population></app-population>
+    </div>
+</template>
+
+<script>
+    import Population from './filters/Population.vue';
+    export default {
+        components: {
+            appPopulation: Population,
+        }
+    }
+</script>
---
+++ client/src/components/filters/Population.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>Population:</h3>
+        <button v-for = "(population, index) in populations" :key = "index">{{ population }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                populations: [
+                "<100,000",
+                ">100,000, < 1,000,000",
+                "> 1,000,000, < 10,000,000",
+                ">10,000,000, <50,000,000",
+                ">50,000,000"
+                ]
+            }
+        }
+    }
+</script>
