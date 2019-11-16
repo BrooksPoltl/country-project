03cd732d9c42b55fb4872f036e05f07d32ac8beb
BrooksPoltl <bpoltl1@gmail.com>
2019-11-16 15:49:13 -0600

    got first filter action working

--- client/src/components/filters/Population.vue
+++ client/src/components/filters/Population.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <h3>Population:</h3>
-        <button v-for = "(population, index) in populations" :key = "index">{{ population }}</button>
+        <button v-for = "(population, index) in populations" :key = "index" @click = "populationFilter(index)">{{ population }}</button>
     </div>
 </template>
 
 <script>
+    import { mapActions } from 'vuex';
     export default {
         data() {
             return {
@@ -17,6 +18,12 @@
                 ">50,000,000"
                 ]
             }
-        }
+        },
+        methods: {
+            ...mapActions([
+                'populationFilter'
+            ])
+        },
+
     }
 </script>
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,28 +2,44 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        countries: []
+        countries: [],
+        currentCountries: [],
     },
     getters: {
         getCountries: state =>{
-            return state.countries;
+            return state.currentCountries;
         }
     },
     mutations: {
         'SET_COUNTRIES' (state, countries) {
             state.countries = countries;
+            state.currentCountries = countries;
+        },
+        'SET_CURRENT_COUNTRIES'(state, countries) {
+            state.currentCountries = countries;
         }
     },
     actions: {
         initCountries: ({ commit }) => {
             Vue.http.get('https://restcountries.eu/rest/v2/all')
             .then(response=> commit('SET_COUNTRIES', response.body))
-           
             
-        }
+            
+        },
+        populationFilter: ({ commit, state }, filterParam) => {
+            
+            let newCountries;
+            switch(filterParam){
+                case 0:
+                    newCountries = state.countries.filter(country => country.population < 100000);
+                    break;
+            }
+            commit('SET_CURRENT_COUNTRIES', newCountries);
+        },
     }
 });
